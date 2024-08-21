b68bfdf50358ceda0ed36c90e53a63d397f286bb
Mansoor-is <mansoorkhanyt76@gmail.com>
2024-08-21 18:36:57 +0500

    update

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio MANSOOR aHMED KHAN AFRIDI";
+            document.title = "Projects | Portfolio Mansoor";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
